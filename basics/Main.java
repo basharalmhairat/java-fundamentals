747ad43c317a0b703f615fee6196353c2087a375
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-02-27 12:51:50 +0300

    labe2+updatelabe1

--- basics/Main.java
+++ basics/Main.java
@@ -2,25 +2,47 @@ package com.company;
 import java.util.Calendar;
 import java.lang.Math;
 public class Main {
-    static String  flipNHeads(int n) {
+//    private static boolean time;
+
+    static String flipNHeads(int n) {
 
         double result = Math.random();
-        if (result < .5){
-            return "tails" ;
-        }
-        else{
+        if (result < .5) {
+            return "tails";
+        } else {
             return "heads";
         }
     }
-        static String  Bluralize(String Animalnam, int nt) {
 
-            if (nt == 1){
-                return Animalnam;
-            }
-            else{
-                return Animalnam+'s';
-            }
+    static String Bluralize(String Animal, int nt) {
+
+        if (nt == 1) {
+            return Animal;
+        } else {
+            return Animal + 's';
         }
+
+    }
+
+    static String Clock() {
+
+        while (true){
+            Calendar x = Calendar.getInstance();
+        String h = Integer.toString(x.get(Calendar.HOUR_OF_DAY));
+        String m = Integer.toString(x.get(Calendar.MINUTE));
+        String s = Integer.toString(x.get(Calendar.SECOND));
+        String time = h + ":" + m + ":" + s;
+        System.out.println(time);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
+
+    }
+
+}
+
+
         public static void main(String[] args) {
             int DogCount = 1;
             System.out.println("I own " + DogCount + " " + Bluralize("dog", DogCount) + ".");
@@ -30,33 +52,24 @@ public class Main {
             System.out.println("I own " + turtleCount + " " + Bluralize("turtle", turtleCount) + ".");
 
 
-            int fliptail = 0;
-            int fliphead = 0;
+            int flipTail = 0;
+            int flipped = 0;
             int flip = 0;
 
-            while (flipNHeads(1) == "tails") {
-                fliptail++;
+            while (flipNHeads(1).equals("tails")) {
+                flipTail++;
                 System.out.println("tails");
             }
 
-            while (flipNHeads(1) == "heads") {
-                fliphead++;
+            while (flipNHeads(1).equals("heads")) {
+                flipped++;
                 System.out.println("heads");
             }
-            flip = fliptail + fliphead;
-            System.out.println("It took" + " " + flip + " " + "flips to flip" + " " + fliphead + " " + "head in a row.");
+            flip = flipTail + flipped;
+            System.out.println("It took" + " " + flip + " " + "flips to flip" + " " + flipped + " " + "head in a row.");
 
+            System.out.println(Clock());
 
-            while (true) {
-                Calendar x = Calendar.getInstance();
-                String s = Integer.toString(x.get(Calendar.SECOND));
-                String m = Integer.toString(x.get(Calendar.MINUTE));
-                String h = Integer.toString(x.get(Calendar.HOUR_OF_DAY));
-                String time = h + ":" + m + ":" + s;
-                System.out.println(time);
-                try {Thread.sleep(1000);} 
-                catch (InterruptedException r) { }
-            }
 
         }
 
